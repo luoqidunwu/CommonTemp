1367e1aecc6b7aff7bcb8282e7d3aa7a48eec0fc
luoqidunwu <luoqidunwu@126.com>
2014-01-26 10:21:56 +0800

    Signed-off-by: luoqidunwu <luoqidunwu@126.com>

--- CommonTemp/src/net/com/bean/Person.java
+++ CommonTemp/src/net/com/bean/Person.java
@@ -52,6 +52,49 @@ public class Person {
 		return "Person [id=" + id + ", name=" + name + ", age=" + age
 				+ ", sex=" + sex + ", date=" + date + "]";
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((sex == null) ? 0 : sex.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (age != other.age)
+			return false;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (sex == null) {
+			if (other.sex != null)
+				return false;
+		} else if (!sex.equals(other.sex))
+			return false;
+		return true;
+	}
 	
 	
 }
